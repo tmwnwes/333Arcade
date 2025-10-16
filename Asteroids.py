333b36f21fce653198e77d6e81953bc7c4a707fc
Jeramiah Trout <jtrout@wesleyan.edu>
2025-10-16 17:24:21 -0400

    New enemy spaceship feature

--- Asteroids.py
+++ Asteroids.py
@@ -35,11 +35,17 @@ app.Yspeed = 0
 app.Xspeed = 0
 app.rotationSpeed = 0
 app.timer = 0
+app.multiplier = 1
 app.asteroidTimer = app.stepsPerSecond*4.5
-app.ballSpeed = 9
+app.ballSpeed = 9 ## Player shots
+app.shotSpeed = 25 ## Enemy shots
 app.launchSpeed = 5
+app.enemyLaunchSpeed = app.stepsPerSecond*3
 app.timeSince = 0
+app.saucerTime = app.stepsPerSecond*9
+app.saucerSpawn = 0
 app.asteroidSpeed = 5
+app.enemy = False
 head = Circle(app.width/2, app.height/2,7, fill = 'white')
 ship = Group(head, Polygon(app.width/2,app.height/2, (81/160)*app.width,(21/40)*app.height, app.width/2,(81/160)*app.height, (79/160)*app.width,(21/40)*app.height, app.width/2,app.height/2, fill='white'))
 ship.health = 3
@@ -49,6 +55,8 @@ asteroids = Group()
 visibleScores = Group()
 explosion = Group()
 gameOver = Group()
+shots = Group()
+saucers = Group()
 
 def update_high_score():
     if app.score>fullInfoList[2]:
@@ -61,6 +69,13 @@ def update_stats():
     gameInfo.seek(0)
     for i in range(len(fullInfoList)):
         gameInfo.write((str)(fullInfoList[i])+"\n")  
+        
+def shots_vs_asteroids():
+    for shot in shots:
+        for ast in asteroids:
+            if(shot.hitsShape(ast) or ast.containsShape(shot)):
+                decrease_health_asteroid(ast, False)
+                shots.remove(shot)
 
 
 def get_speed(speed):
@@ -75,6 +90,19 @@ def spawn_balls(x,y,angle):
     new.next = getPointInDir(x,y,angle,app.ballSpeed)
     balls.add(new)
     fullInfoList[0]+=1
+    
+def spawn_shots(x,y,angle):
+    new = Oval(x,y,2,20, fill='white', rotateAngle = angle)
+    new.next = getPointInDir(x,y,angle,app.shotSpeed)
+    new2 = Oval(x,y,2,20, fill='white', rotateAngle = angle+10)
+    new2.next = getPointInDir(x,y,angle+10,app.shotSpeed)
+    new3 = Oval(x,y,2,20, fill='white', rotateAngle = angle-10)
+    new3.next = getPointInDir(x,y,angle-10,app.shotSpeed)
+    new4 = Oval(x,y,2,20, fill='white', rotateAngle = angle+5)
+    new4.next = getPointInDir(x,y,angle+5,app.shotSpeed)
+    new5 = Oval(x,y,2,20, fill='white', rotateAngle = angle-5)
+    new5.next = getPointInDir(x,y,angle-5,app.shotSpeed)
+    shots.add(new, new2, new3, new4, new5)
     
 def create_scores(x,y,val):
     visibleScores.add(Label("+ %3d" %val, x, y, fill=None, border = 'white', size = 14))
@@ -154,17 +182,76 @@ def move_asteroids():
         ast.centerX, ast.centerY = ast.next
         ast.next = getPointInDir(ast.centerX, ast.centerY, ast.rotateAngle, ast.speed)
     
-def spawn_enemy_saucer():
-    None ## To be implemented later
+def create_saucer(left, top, right, bottom):
+    new = Group()
+    new.health = app.multiplier
+    new.score = 500*app.multiplier
+    saucerTop = Arc(100,100, 80, 40, 270, 180, fill=None, border = 'white')
+    saucerMid = Rect(55, 100, 90, 10, fill="gray")
+    saucerBottom = Arc(100, 110, 90, 20, 90, 180, fill='white')
+    randX = randrange((int)(left), (int)(right))
+    randY = randrange((int)(top), (int)(bottom))
+    new.add(saucerTop, saucerMid, saucerBottom)
+    new.centerX = randX
+    new.centerY = randY
+    app.saucerSpawn = 0
+    return new 
     
-def enemy_firing():
-    None ## To be implemented later
+    
+def shots_vs_ship():
+    for shot in shots:
+        if(shot.hitsShape(ship)):
+            shots.remove(shot)
+            decrease_health_ship()
+    
+def spawn_enemy_saucer():
+    location = randrange(4)
+    saucer = None
+    if(location == 0):
+        saucer = create_saucer(0,0,app.width/8, app.height/8)
+    elif(location == 1):
+        saucer = create_saucer(7* app.width/8, 0, app.width, app.height/8)
+    elif(location == 2):
+        saucer = create_saucer(0,7*app.height/8, app.width/8, app.height)
+    else:
+        saucer = create_saucer(7*app.width/8, 7*app.height/8, app.width, app.height)
+    saucers.add(saucer)
+    app.enemy = True
+    
+def enemy_firing(saucer):
+    if(app.enemy == True and app.timer%app.enemyLaunchSpeed == 0):
+        spawn_shots(saucer.centerX, saucer.centerY, angleTo(saucer.centerX, saucer.centerY, ship.centerX, ship.centerY))
+    
     
 def move_enemy_shots():
-    None ## To be implemented later
+    for shot in shots:
+        shot.centerX, shot.centerY = shot.next
+        shot.next = getPointInDir(shot.centerX,shot.centerY,shot.rotateAngle, app.shotSpeed)
+        if(not(screen.containsShape(shot))):
+            shots.remove(shot)
     
-def balls_versus_enemy():
-    None ## To be implemented later
+    
+def decrease_health_saucer(saucer):
+    if saucer.health<=1:
+        create_scores(saucer.centerX, saucer.centerY, saucer.score)
+        app.score+=saucer.score
+        explosion.add(Star(saucer.centerX, saucer.centerY, 12, 12, fill = None, border = 'white'))
+        explosion.add(Star(saucer.centerX, saucer.centerY, 8, 12, fill = None, border = 'white'))
+        explosion.add(Star(saucer.centerX, saucer.centerY, 4, 12, fill = None, border = 'white'))
+        saucers.remove(saucer)
+        if(len(saucers) ==0):
+            app.enemy = False
+    else:
+        saucer.health-=1
+    
+def balls_vs_enemy():
+    for saucer in saucers:
+        for ball in balls:
+            if(saucer.hitsShape(ball)):
+                explosion.add(Circle(ball.centerX, ball.centerY, 3, fill=None, border = 'white'))
+                balls.remove(ball)
+                fullInfoList[1]+=1
+                decrease_health_saucer(saucer)
     
     
 def spawn_asteroids(num):
@@ -185,8 +272,12 @@ def spawn_asteroids(num):
             asteroids.add(massive(randX, randY))
         
 def reset():
+    app.timer = 0
+    app.saucerSpawn = 0
+    shots.clear()
     asteroids.clear()
     balls.clear()
+    saucers.clear()
     visibleScores.clear()
     explosion.clear()
     gameOver.clear()
@@ -199,8 +290,10 @@ def reset():
     ship.rotateAngle = 0
     app.Xspeed = 0
     app.Yspeed = 0
+    app.multiplier = 1
     ship.health = 3
     health.value = "Health: %1d" %ship.health
+    app.enemy = False
     update_stats()
 
     
@@ -229,9 +322,13 @@ def decrease_health_ship():
         ship.health-=1
     health.value = "Health %1d" %ship.health
 
-def decrease_health_asteroid(ast):
+def decrease_health_asteroid(ast, scoring):
     if ast.health<=1:
-        create_scores(ast.centerX, ast.centerY, ast.score)
+        if(scoring == True):
+            create_scores(ast.centerX, ast.centerY, ast.score*app.multiplier)
+            app.score+=ast.score * app.multiplier
+        else:
+            create_scores(ast.centerX, ast.centerY, 0)
         if(ast.size == 'massive'):
             asteroids.add(big(ast.centerX, ast.centerY))
             asteroids.add(big(ast.centerX, ast.centerY))
@@ -241,7 +338,6 @@ def decrease_health_asteroid(ast):
         if(ast.size == 'med'):
             asteroids.add(small(ast.centerX, ast.centerY))
             asteroids.add(small(ast.centerX, ast.centerY))
-        app.score+=ast.score
         asteroids.remove(ast)
     else:
         ast.health-=1
@@ -267,7 +363,7 @@ def balls_vs_asteroids():
                 explosion.add(Circle(ball.centerX, ball.centerY, 3, fill=None, border = 'white'))
                 balls.remove(ball)
                 fullInfoList[1]+=1
-                decrease_health_asteroid(ast)
+                decrease_health_asteroid(ast, True)
                 
 
 def asteroids_vs_space():
@@ -285,6 +381,9 @@ def hit_detection():
     ship_vs_asteroids()
     balls_vs_asteroids()
     asteroids_vs_space()
+    balls_vs_enemy()
+    shots_vs_ship()
+    shots_vs_asteroids()
 
 
 def onKeyHold(keys):
@@ -305,27 +404,41 @@ def onKeyHold(keys):
             spawn_balls(head.centerX, head.centerY, ship.rotateAngle%360)
             app.timeSince = app.launchSpeed
         
-def onStep():
+def onStep():  
     if(app.play==True):
+        app.timer+=1
+        if(app.timer%(app.stepsPerSecond*app.multiplier*60) == 1):
+            for i in range(app.multiplier):
+                if(i<=3):
+                    spawn_enemy_saucer()
+                    app.saucerSpawn= 0
+            app.multiplier += 1
         if(app.timeSince>0):
             app.timeSince -=1
-        app.timer+=1
         app.Xspeed = get_speed(app.Xspeed)
         ship.centerX+=app.Xspeed
         app.Yspeed = get_speed(app.Yspeed)
         ship.centerY+=app.Yspeed
         app.rotationSpeed = get_speed(app.rotationSpeed)
         ship.rotateAngle+=app.rotationSpeed
-        app.score+=1
+        app.score+=app.multiplier
         score.value = "Score: %09d" %app.score
-        if(app.timer%app.asteroidTimer==0):
+        if(app.timer%app.asteroidTimer==0 and app.enemy==False):
             spawn_asteroids(randrange(4,10))
+        for saucer in saucers:
+            enemy_firing(saucer)
+        app.saucerSpawn+=1
+        if(app.saucerSpawn >= app.saucerTime and app.enemy == True):
+            app.saucerSpawn = 0
+            spawn_enemy_saucer()
         wrap_around()
         move_balls()
         move_asteroids()
         hit_detection()
         remove_scores()
         blow_up_balls()
+        move_enemy_shots()
+        shots_vs_ship()
         update_high_score()
         update_stats()
         
@@ -346,6 +459,8 @@ def wrap_around():
         elif(ship.centerY>app.height):
             ship.centerY = 0
             
+score.toFront()
+hiScore.toFront()
 fullInfoList[4]+=1
 fullInfoList[3]+=1     
     
