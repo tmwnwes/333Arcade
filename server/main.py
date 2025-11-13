ace5016c3a024220c5a1b863664b17d9c4acaf2e
tmwnwes <tmwatson@wesleyan.edu>
2025-11-13 16:02:04 -0500

    update

---
+++ server/main.py
@@ -0,0 +1,16 @@
+'''
+This server will contain program info including: 
+
+1. news and or program update information
+2. user profiles/user data
+3. program info such as desciptions or tags
+--> this info could be cached locally
+4. ratings
+5. comments?
+6. moderation system on comments?
+
+'''
+
+
+
+
