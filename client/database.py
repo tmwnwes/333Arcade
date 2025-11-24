7ac313772a358fd69242b9fc085556d2ca039323
tmwnwes <tmwatson@wesleyan.edu>
2025-11-24 16:36:31 -0500

    update

---
+++ client/database.py
@@ -0,0 +1,2 @@
+import sqlite3
+from pathlib import Path
---
+++ client/metadata.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+import toml
+from pathlib import Path
+
+class ProgramMetadata(BaseModel):
+    id: int
+    name: str
+    version: str | None = None
+    exePath: str
+
+def load_metadata(path:Path) -> ProgramMetadata:
+    data = toml.load(path)
+    return ProgramMetadata(**data)
